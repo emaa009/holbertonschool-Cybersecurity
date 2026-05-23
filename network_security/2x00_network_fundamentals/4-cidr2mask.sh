fb96680641ecf2546724f34552b7741fea8b3056
emaa009 <elmira.arifzade@gmail.com>
2026-05-23 17:35:28 +0000

    fixed

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$((0xFFFFFFFF << (32 - $1))); for i in {3..0}; do printf "%d" $(( (mask >> (i * 8)) & 255 )); [ $i -ne 0 ] && printf "."; done; printf "\n"
+mask=$((0xFFFFFFFF << (32 - $1))); for i in {3..0}; do printf "%d" $(( (mask >> (i * 8)) & 255 )); [ $i -ne 0 ] && printf "."; done
